b59bae45cf934dd64259b5bd5c26f655443ad0e9
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-10-09 01:19:48 -0300

    fix: reajuste de componente para exibiçã em mobile

--- src/app/components/supportersSection.tsx
+++ src/app/components/supportersSection.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-// Define a interface para o tipo dos itens de supporters
 interface Supporter {
   logoUrl: string;
   alt: string;
@@ -27,16 +26,23 @@ const SupportersSection = ({ supporters }: SupportersSectionProps) => {
     arrows: true, 
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 480, 
         settings: {
-          slidesToShow: 1, 
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768, 
+        settings: {
+          slidesToShow: 2, 
           slidesToScroll: 1,
         },
       },
       {
         breakpoint: 1024, 
         settings: {
-          slidesToShow: 2, 
+          slidesToShow: 3, 
           slidesToScroll: 1,
         },
       },
