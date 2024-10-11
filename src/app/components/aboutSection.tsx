4f0982a3abcd7d61bcbeb3033c48de8726f834a6
isaacfelipe1 <ifdsl.lic20@uea.edu.br>
2024-10-11 17:31:08 -0300

    feat: adiconado o botao de conheça mais

--- src/app/components/aboutSection.tsx
+++ src/app/components/aboutSection.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 
 type AboutSectionProps = {
@@ -12,20 +13,54 @@ const AboutSection: React.FC<AboutSectionProps> = ({
   aboutDescription2,
 }) => {
   return (
-    <section className="relative py-12" style={{ marginTop: '50px' }}>
-      <div className="container mx-auto px-4 lg:px-8 lg:max-w-4xl shadow-lg rounded-lg backdrop-blur-md bg-transparent">
-        <h3 className="text-[#005226] text-sm uppercase font-bold tracking-wide mb-4">
-          SOBRE a biblioetca
+    <section
+      className="relative py-12 lg:py-16 bg-transparent"
+      style={{ marginTop: '50px' }}
+      aria-label="Sobre a Biblioteca"
+    >
+      <div
+        className="container mx-auto px-4 lg:px-8 lg:max-w-4xl shadow-lg rounded-lg backdrop-blur-md bg-white transition-all duration-500 ease-in-out"
+        data-aos="fade-up"
+        data-aos-delay="100"
+      >
+        <h3
+          className="text-[#005226] text-sm uppercase font-bold tracking-wide mb-4"
+          aria-label="Sobre a biblioteca"
+        >
+          SOBRE A BIBLIOTECA
         </h3>
-        <h2 className="text-[#005226] text-3xl lg:text-4xl font-bold mb-6">
+        <h2
+          className="text-[#005226] text-3xl lg:text-4xl font-bold mb-6"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        >
           {aboutTitle}
         </h2>
-        <p className="text-lg text-gray-700 mb-4 leading-relaxed">
+        <p
+          className="text-lg text-gray-800 mb-4 leading-relaxed lg:leading-loose"
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
           {aboutDescription1}
         </p>
-        <p className="text-lg text-gray-700 leading-relaxed">
+        <p
+          className="text-lg text-gray-800 leading-relaxed lg:leading-loose"
+          data-aos="fade-up"
+          data-aos-delay="400"
+        >
           {aboutDescription2}
         </p>
+        <div className="mt-6">
+          <a
+            href="../pages/como-comecou"
+            className="bg-green-600 text-white py-3 px-6 rounded-full hover:bg-green-700 transition-colors duration-300 transform hover:scale-105 shadow-lg"
+            aria-label="Saiba mais sobre a biblioteca"
+            data-aos="fade-up"
+            data-aos-delay="500"
+          >
+            Conheça como tudo começou
+          </a>
+        </div>
       </div>
     </section>
   );
